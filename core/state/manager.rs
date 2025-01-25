61aef04f7330ef343dd0e11f0b00d9f957438875
urizennnn <igamerryt@gmail.com>
2025-01-25 17:32:31 +0100

    chore:fixed progress drawing

--- core/state/manager.rs
+++ core/state/manager.rs
@@ -1,8 +1,5 @@
 use super::state::{ScreenState, StateSnapshot};
-use crate::{
-    screens::{dashboard::table_ui, home::Home, session::draw_session_table_ui},
-    utils::poll::poll_future,
-};
+use crate::screens::{dashboard::table_ui, home::Home, session::draw_session_table_ui};
 use futures::lock::Mutex;
 use ratatui::DefaultTerminal;
 use std::sync::Arc;
@@ -12,55 +9,56 @@ pub async fn manage_state(
     state_snapshot: Arc<Mutex<StateSnapshot<'_>>>,
     term: Arc<Mutex<DefaultTerminal>>,
 ) -> Result<(), std::io::Error> {
-    let progress = Arc::clone(state_snapshot.lock().await.progress);
-
-    tokio::spawn(async move {
-        let mut progress = progress.lock().await;
-        progress.update().await;
-    });
-
     let mut state_guard = state_snapshot.lock().await;
+    tokio::spawn(async move {});
 
-    term.lock().await.draw(|frame| match home.current_screen {
-        ScreenState::Sessions => {
-            draw_session_table_ui(frame, state_guard.table);
-            if state_guard.table.help {
-                state_guard.help.draw_dashboard_help(frame);
+    if home.current_screen == ScreenState::TcpServer {
+        let progress = Arc::clone(state_guard.progress);
+        let term_clone = Arc::clone(&term);
+
+        tokio::spawn(async move {
+            let mut guard = progress.lock().await;
+            term_clone
+                .lock()
+                .await
+                .draw(|frame| {
+                    guard.draw(frame);
+                })
+                .unwrap();
+        });
+    }
+
+    term.lock().await.draw(|frame| {
+        let state = &mut *state_guard;
+
+        match home.current_screen {
+            ScreenState::Sessions => {
+                draw_session_table_ui(frame, state.table);
+                if state.table.help {
+                    state.help.draw_dashboard_help(frame);
+                }
             }
-        }
-        ScreenState::Transfer => {
-            table_ui(frame, state_guard.table);
-            if state_guard.table.help {
-                state_guard.help.draw_dashboard_help(frame);
+            ScreenState::Transfer => {
+                table_ui(frame, state.table);
+                if state.table.help {
+                    state.help.draw_dashboard_help(frame);
+                }
             }
-        }
-        ScreenState::Connection => {
-            state_guard.table.active = false;
-            if state_guard.connection.visible {
-                state_guard.connection.render(frame);
+            ScreenState::Connection => {
+                state.table.active = false;
+                if state.connection.visible {
+                    state.connection.render(frame);
+                }
             }
-        }
-        ScreenState::TCP => {
-            state_guard.connection.visible = false;
-            state_guard.table.active = false;
-            if state_guard.host.visible {
-                state_guard.host.render(frame);
+            ScreenState::TCP => {
+                state.connection.visible = false;
+                state.table.active = false;
+                if state.host.visible {
+                    state.host.render(frame);
+                }
             }
+            _ => {}
         }
-        ScreenState::TcpServer => {
-            let progress = poll_future(Box::pin(state_guard.progress.lock()));
-            progress.draw(frame);
-        }
-        // ScreenState::TcpClient => {
-        //     if state_guard.connection.input_popup {
-        //         state_guard.connection.draw_input(
-        //             frame,
-        //             state_guard.connection.returned_val,
-        //             state_guard.input_box,
-        //         );
-        //     }
-        // }
-        _ => {}
     })?;
 
     Ok(())
